876cfbb344ef25ac61a2b22c1aa7bab75344d9b2
tong <tong@disktree.net>
2022-05-12 22:22:05 +0200

    Init kyria

---
+++ keyboards/splitkb/kyria/keymaps/tong/rules.mk
@@ -0,0 +1,9 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = yes       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+UNICODE_ENABLE = no
+#WPM_ENABLE = yes
+# COMBO_ENABLE =  yes
+# TAP_DANCE_ENABLE = yes
+#VIA_ENABLE = yes
