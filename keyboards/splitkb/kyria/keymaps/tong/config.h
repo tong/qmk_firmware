bc3cea31d123c80d6fc718168094a2853fb60119
tong <tong@disktree.net>
2022-06-08 19:10:07 +0200

    ...

--- keyboards/splitkb/kyria/keymaps/tong/config.h
+++ keyboards/splitkb/kyria/keymaps/tong/config.h
@@ -1,50 +1,31 @@
-/* Copyright 2022 Thomas Baart <thomas@splitkb.com>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
 #pragma once
 
+#define LEADER_PER_KEY_TIMING
+#define LEADER_TIMEOUT 250
+
+//#define TAPPING_TERM 200
+//#define IGNORE_MOD_TAP_INTERRUPT
+
+#ifdef ENCODER_ENABLE
+//#define ENCODER_RESOLUTION  2
+#define ENCODER_DIRECTION_FLIP
+#endif
+
 #ifdef OLED_ENABLE
-    //#define OLED_DISPLAY_128X64
-    #define OLED_TIMEOUT 10000
-    //#define OLED_FADE_OUT
-    //#define OLED_FADE_OUT_INTERVAL 15
-    //#define OLED_BRIGHTNESS 23
-    //#define OLED_FONT_H "keyboards/splitkb/kyria/keymaps/benji/glcdfont.c"
+#define OLED_DISPLAY_128X64
+#define OLED_TIMEOUT 60000
+//#define OLED_FADE_OUT
+//#define OLED_FADE_OUT_INTERVAL 0x0f
+#define OLED_BRIGHTNESS 255
+#define OLED_FONT_H "keyboards/splitkb/kyria/keymaps/tong/glcdfont.c"
 #endif
 
 #ifdef RGBLIGHT_ENABLE
-#    define RGBLIGHT_ANIMATIONS
-#    define RGBLIGHT_HUE_STEP  8
-#    define RGBLIGHT_SAT_STEP  8
-#    define RGBLIGHT_VAL_STEP  8
-#    define RGBLIGHT_LIMIT_VAL 150
+#define RGBLIGHT_SLEEP
+#define RGBLIGHT_LIMIT_VAL 200
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_SPLIT
 #endif
 
-// Lets you roll mod-tap keys
-#define IGNORE_MOD_TAP_INTERRUPT
-
-#ifdef ENCODER_ENABLE
-  // EC11K encoders have a different resolution than other EC11 encoders.
-  // When using the default resolution of 4, if you notice your encoder skipping
-  // every other tick, lower the resolution to 2.
-  #define ENCODER_RESOLUTION 2
-  // Also, flip direction
-  #define ENCODER_DIRECTION_FLIP
-#endif
-
-// If you are using an Elite C rev3 on the slave side, uncomment the lines below:
-//#define SPLIT_USB_DETECT
-//#define NO_USB_STARTUP_CHECK
--- keyboards/splitkb/kyria/keymaps/tong/rules.mk
+++ keyboards/splitkb/kyria/keymaps/tong/rules.mk
@@ -1,11 +1,43 @@
 OLED_ENABLE = yes
-OLED_DRIVER = SSD1306   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
-RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-UNICODE_ENABLE = no
-#WPM_ENABLE = yes
+ENCODER_ENABLE = yes
+LEADER_ENABLE = yes
+WPM_ENABLE = yes
 # COMBO_ENABLE =  yes
-# TAP_DANCE_ENABLE = yes
-#VIA_ENABLE = yes
+#RAW_ENABLE = yes
 
-SRC += oled.c
+VIA_ENABLE = no
+UNICODE_ENABLE = no
+RGBLIGHT_ENABLE = no
+MOUSEKEY_ENABLE = no
+TAP_DANCE_ENABLE = no
+STENO_ENABLE = no
+BOOTMAGIC_ENABLE = no
+TERMINAL_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+SPACE_CADET_ENABLE = no
+KEY_LOCK_ENABLE = no
+AUDIO_ENABLE = no
+CONSOLE_ENABLE = no
+VELOCIKEY_ENABLE = no
+
+#SRC += gen.c
+
+#ifeq ($(strip $(LEADER_ENABLE)), yes)
+	 #SRC += leader.c
+#endif
+
+ifeq ($(strip $(ENCODER_ENABLE)), yes)
+	SRC += encoder_utils.c
+	#SRC += encoder.c
+endif
+
+#ifeq ($(strip $(OLED_ENABLE)), yes)
+	#SRC += oled.c
+#endif
+
+#ifeq ($(strip $(RAW_ENABLE)), yes)
+	#SRC += hid.c
+#endif
+
+
